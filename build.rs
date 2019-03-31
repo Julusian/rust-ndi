1751b3fcecd7330d96d788fa430d5e3b7d79e7fa
Julian Waller <git@julusian.co.uk>
2019-03-31 18:16:14 +0100

    feat: Initial commit

---
+++ build.rs
@@ -0,0 +1,8 @@
+// build.rs
+
+fn main() {
+    println!("cargo:rustc-link-lib=ndi");
+    println!("cargo:rustc-link-search=native=input");
+
+    //println!("done");
+}
---
+++ gen-bindings.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bindgen input/Processing.NDI.Lib.h -o src/sdk.rs
